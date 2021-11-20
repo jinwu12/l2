060495153763a8ac3adc75835728f0555fa8c53d
Weijian Liao <jewforice@gmail.com>
2021-11-20 04:59:09 +0000

    单个品类原始数据表模板，分钟级及小时级原始数据均使用此模板进行新建表存储

--- Db_SQL/original_data_source.single_symbol_original_data_template.sql
+++ Db_SQL/original_data_source.single_symbol_original_data_template.sql
@@ -1,10 +1,9 @@
 create table single_symbol_original_data_template(
-symbol_name varchar(32) not null '单个品类名称',
-timestamp bigint not null PRIMARY KEY '时间点，所有粒度数据均需要转换为unix timestamp后再入库',
-open float not null '开盘价',
-high float not null '最高价',
-low float  not null '最低价',
-closed float not null '收盘价',
-comment text default null
+symbol_name VARCHAR(16) NOT NULL comment '品类名称',
+ts bigint(64) unsigned NOT NULL comment '时间点数据均需要转换为unix timestamp后再入库',
+price_open DOUBLE NOT NULL comment '开盘价',
+price_hgih DOUBLE NOT NULL comment '最高价',
+price_low DOUBLE NOT NULL comment '最低价',
+price_closed DOUBLE NOT NULL comment '收盘价',
+comments TEXT comment '备注字段'
 );
- 
