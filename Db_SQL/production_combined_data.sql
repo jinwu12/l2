996e8f5a729fa88a2bc9bb126d769ce799645e81
Weijian Liao <jewforice@gmail.com>
2021-11-20 05:28:47 +0000

    新增日线组合价格表模板，到实际要写生成入库的程序时再生成表，表名示例如下： (production | simunation)_daily_combined_data_$combination_id_$timezone_id

---
+++ Db_SQL/production_combined_data.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: production_combined_data
+-- ------------------------------------------------------
+-- Server version	8.0.27-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `daily_combined_data_template`
+--
+
+DROP TABLE IF EXISTS `daily_combined_data_template`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `daily_combined_data_template` (
+  `combination_id` int unsigned NOT NULL COMMENT '交易组合id，用于标识该日线行情记录表为哪个交易组合的数据',
+  `ts` bigint unsigned NOT NULL COMMENT '时间点数据均需要转换为unix timestamp后再入库',
+  `timezone_id` int unsigned NOT NULL COMMENT '时区id，用于标识该表使用的是哪个时区',
+  `price_closed` double NOT NULL COMMENT '以该时区为准的日线收盘价',
+  `is_recorded` tinyint(1) NOT NULL COMMENT '该价格是否需要在行情记录表中进行显性显示，0为小幅波动，不需要；1为需要',
+  `recorded_column` tinyint unsigned NOT NULL COMMENT '当前记录栏。0：不记录；1：次级回升栏；2：自然回升栏；3：上升趋势栏；4：下降趋势栏；5：自然回撤栏；6：次级回撤栏',
+  `is_pivot` tinyint(1) NOT NULL COMMENT '该点是否关键点,1为是,0为否',
+  `comments` text COMMENT '备注字段'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `daily_combined_data_template`
+--
+
+LOCK TABLES `daily_combined_data_template` WRITE;
+/*!40000 ALTER TABLE `daily_combined_data_template` DISABLE KEYS */;
+/*!40000 ALTER TABLE `daily_combined_data_template` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-20  5:28:19
