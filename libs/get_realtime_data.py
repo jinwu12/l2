5bafb4b5429333dfe039aefccc7f4d11c875560d
weijianliao <jewforice@gmail.com>
2021-12-14 12:21:39 +0800

    已完成schedule函数update_realtime_data的开发，测试通过后开始编写调度器

--- libs/get_realtime_data.py
+++ libs/get_realtime_data.py
@@ -1,10 +1,105 @@
 import math
 import time
-import datetime
+from datetime import datetime, date, time, timezone
 import pandas as pd
 import sys
+import pytz
 sys.path.append('..')
 from libs import get_historical_data
+from libs import commons
+
+
+
+#封装入库实时数据schedule函数              
+def update_realtime_data(interval,db,mt5_account,mt5):                                             
+    #初始化数据库连接
+    db = commons.db_connect()
+    method_list = get_historical_data.get_symbol_method(db)
+    #获取symbol method对应的method list，并进行遍历
+    for symbol_method in method_list:
+        symbol_name = symbol_method[0]
+        #根据symbol name获取拉取数据用的symbol value，用于获取数据
+        symbol_value = commons.get_symbol_value(symbol_name,db)
+        method = symbol_method[1]
+        timezone = symbol_method[2]
+        yf_rates = []
+        mt5_rates = []
+        dxy_rates = []
+        #用于保存每种计算方式的字典
+        data_dict = {'interval': interval}
+        data_dict = {'symbol': symbol_name}
+        data_dict = {'timezone': timezone}
+        data_dict['method'] = method
+        #结果列表
+        result_list = []
+        #拉取yfinance数据源的symbol数据
+        if method == 'get_historical_data_from_yfinance':
+            #生成时间间隔，必需按照时区转换时间后，按照隔日进行拉取
+            yf_tz = pytz.timezone(timezone)
+            yf_start_time = yf_tz.localize(date.today())
+            yf_end_time = yf_start_time + timedelta(days=1)
+            #将method附加到结果中
+            #yfinance的分钟级及小时级数据拉取逻辑一致
+            #拉取数据，并截取最后一个元素作为结果; 因为入库时executemany需要外侧是list+内层是tuple，所以此处需要用append而不能直接赋值
+                yf_rates.append(get_historical_data.get_historical_data_from_yfinance(symbol_value,interval,yf_start_time,yf_end_time,timezone)[-1])
+                #附加到data_dict中
+                data_dict['value'] = yf_rates
+                #数据入库
+                commons.insert_historical_original_data_to_db(symbol_name,yf_rates,interval,db)
+        #拉取mt5数据的symbol数据
+        if method == 'get_historical_data_from_mt5':
+            #获取当前的utc时间
+            current_time = now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+            #生成转换为UTC时区后的开始时间
+            mt5_start_time = pd.to_datetime(current_time)
+            #分钟级的逻辑
+            if interval ='1m':
+                #生成转换为UTC时区后的时间间隔
+                mt5_start_time = pd.to_datetime(current_time)
+                #对于XAUUSD等非外汇货币对，结束时间需要+1个interval
+                #如果涉及有其他结束时间需要+1个interval的symbol，此处需要修改，可以考虑在db中新增interval_type进行配置
+                if symbol_value == 'XAUUSD':
+                    mt5_end_time = mt5_start_time + timedelta(minutes=1)
+                else:
+                    mt5_end_time = mt5_start_time
+                #拉取数据
+                mt5_rates = get_historical_data.get_historical_data_from_mt5(symbol_value,mt5.TIMEFRAME_M1,mt5_start_time,mt5_end_time,mt5_account,db,mt5)
+                #附加到data_dict中
+                data_dict['value'] = mt5_rates
+            #小时级的逻辑
+            if interval ='1h':
+                #对于XAUUSD等非外汇货币对，结束时间需要+1个interval
+                if symbol_value == 'XAUUSD':
+                    mt5_end_time = mt5_start_time + timedelta(hours=1)
+                else:
+                    mt5_end_time = mt5_start_time
+                #拉取数据
+                mt5_rates = get_historical_data.get_historical_data_from_mt5(symbol_value,mt5.TIMEFRAME_H1,mt5_start_time,mt5_end_time,mt5_account,db,mt5)
+                #附加到data_dict中
+                data_dict['value'] = mt5_rates
+            #数据入库
+            commons.insert_historical_original_data_to_db(symbol_name,mt5_rates,interval,db)
+        #从mt5拉取数据去生成的symbol数据,当前只有dxy，如果有需要就继续在此分支下添加if即可
+        if method == 'originate_from_mt5':
+            #获取当前的utc时间
+            current_time = now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+            #根据mt5等货币对报价，生成DXY
+            if symbol_value == 'DXY-MT5':
+                #生成转换为UTC时区后的开始时间
+                mt5_start_time = current_time
+                #由于DXY报价必然不会涉及贵金属报价，所以直接赋值为与开始时间相等即可
+                mt5_end_time = mt5_start_time
+                #拉取并计算dxy
+                dxy_rates = get_dxy_from_mt5(mt5_start_time,mt5_end_time,interval,mt5_account,db,mt5)
+                #附加到data_dict中
+                data_dict['value'] = dxy_rates
+                #数据入库
+                commons.insert_historical_original_data_to_db(symbol_value,dxy_rates,interval,db)
+        result_list.append(data_dict)
+    return result_list
+
+
+
 
 #根据特定时间点的美元指数成分货币对报价，计算该时间点的美元指数报价
 def calculate_dxy(symbols_rate_list):
@@ -73,7 +168,7 @@ def get_dxy_from_mt5(start,end,interval,account,db,mt5):
             exit()
         else:
             #初始化开始时间和结束时间
-            start_time = datetime.datetime.strptime(start_rounding_time,"%Y-%m-%d %H:%M:%S")
+            start_time = datetime.strptime(start_rounding_time,"%Y-%m-%d %H:%M:%S")
             #拉取开始时间这个时间戳的数据，开始和结束时间一致即可
             end_time = start_time
             for minute in range(0,delta_minute):
@@ -89,7 +184,7 @@ def get_dxy_from_mt5(start,end,interval,account,db,mt5):
                         skip = True
                         break
                 #每计算1分钟的dxy之后，需要将时间往前挪1分钟
-                start_time = start_time+ datetime.timedelta(minutes=1)
+                start_time = start_time+ timedelta(minutes=1)
                 end_time = start_time
                 result_list.append(tuple(calculate_dxy(symbols_rate_list)))
                 #计算完毕后，清空这一个周期内的symbols_rate_list
@@ -106,7 +201,7 @@ def get_dxy_from_mt5(start,end,interval,account,db,mt5):
             exit()
         else:
             #初始化开始时间和结束时间
-            start_time = datetime.datetime.strptime(start_rounding_time,"%Y-%m-%d %H:%M:%S")
+            start_time = datetime.strptime(start_rounding_time,"%Y-%m-%d %H:%M:%S")
             #拉取开始时间这个时间戳的数据，开始和结束时间一致即可
             end_time = start_time
             for hour in range(0,delta_hours):
@@ -129,7 +224,7 @@ def get_dxy_from_mt5(start,end,interval,account,db,mt5):
                 #将内层转为tuple，方便后续使用mysql connector的excutemany入库
                 result_list.append(tuple(dxy_result_list))
                 #每计算1个小时的dxy之后，需要将时间往前挪1小时
-                start_time = start_time+ datetime.timedelta(hours=1)
+                start_time = start_time+ timedelta(hours=1)
                 end_time = start_time
                 #计算完毕后，清空这一个周期内的symbols_rate_list
                 symbols_rate_list = []
