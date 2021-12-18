bbde5470711f709307acf438ad9b0f67d371e6fd
weijianliao <jewforice@gmail.com>
2021-12-18 17:46:18 +0800

    1. 将调度器修改为apscheduler 2. 运行时间为周日晚上23点-周六凌晨3点。 3. 目前已在运行，待周一开始观察是否正常。

--- get_source_realtime_data.py
+++ get_source_realtime_data.py
@@ -1,39 +1,76 @@
 from libs import commons
-import schedule
 import MetaTrader5 as mt5
 from libs import get_realtime_data
-import time
 from apscheduler.schedulers.background import BackgroundScheduler
+import time
 
 #初始化入库db
 db = commons.db_connect()
 
 #初始化后台调度器
-scheduler = BackgroundScheduler()
-scheduler.start()
+scheduler = BackgroundScheduler(timezone='Asia/Shanghai')
 
 #添加调度器任务
 #每周周日23时0分0秒-周六凌晨2点，始执行实时更新method list中所有symbol的入库任务
 #周日23点-周日0点
 #分钟级数据
-#小时级数据
-#周六0点-周六2点
-#分钟级数据
 scheduler.add_job(
-        get_realtime_data.update_realtime_data(),
+        get_realtime_data.update_realtime_data,
         trigger='cron',
-        day_of_week = '6',
-        hour='0-2',
-        minute='*/1',
-        args=['1m',db,'5348288',mt5]
+        day_of_week = 'sun',
+        hour='23',
+        minute='0-59',
+        args = ['1m',db,'5348288',mt5]
 )
 #小时级数据
+scheduler.add_job(
+        get_realtime_data.update_realtime_data,
+        trigger='cron',
+        day_of_week = 'sun',
+        hour='23',
+        args = ['1h',db,'5348288',mt5]
+)
 
-'''
-#每小时执行入库任务
-schedule.every(1).hour.at('00:01').do(get_realtime_data.update_realtime_data,interval='1h',db=db,mt5_account='5348288',mt5=mt5)
+#周一0点～周五23点
+#分钟级数据
+scheduler.add_job(
+        get_realtime_data.update_realtime_data,
+        trigger='cron',
+        day_of_week = 'mon-fri',
+        hour = '0-23',
+        minute='0-59',
+        args = ['1m',db,'5348288',mt5]
+)
+#小时级数据
+scheduler.add_job(
+        get_realtime_data.update_realtime_data,
+        trigger='cron',
+        day_of_week = 'mon-fri',
+        hour = '0-23',
+        args = ['1h',db,'5348288',mt5]
+)
 
-while True:
-    schedule.run_pending()
+#周六0点-周六3点
+#分钟级数据
+scheduler.add_job(
+        get_realtime_data.update_realtime_data,
+        trigger='cron',
+        day_of_week = 'sat',
+        hour='0-3',
+        minute='0-59',
+        args = ['1m',db,'5348288',mt5]
+)
+#小时级数据
+scheduler.add_job(
+        get_realtime_data.update_realtime_data,
+        trigger='cron',
+        day_of_week = 'sat',
+        hour='0-3',
+        args = ['1h',db,'5348288',mt5]
+)
+
+#启动调度器
+scheduler.start()
+
+while(True):
     time.sleep(1)
-'''
