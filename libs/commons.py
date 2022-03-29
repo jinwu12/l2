0dbd65ec5f1402028e44b1197778a276962cd487
weijianliao <jewforice@gmail.com>
2022-03-29 21:10:36 +0800

    在默认的日志文件名中增加进程ID进行区分，避免每周补录任务和实时任务的日志文件有冲突

--- libs/commons.py
+++ libs/commons.py
@@ -4,6 +4,7 @@ import logging.config
 import mysql.connector
 from configparser import ConfigParser
 from datetime import datetime
+import os
 
 import pytz
 
@@ -20,7 +21,7 @@ def create_logger(name='app'):
         logger.setLevel(logging.INFO)
         log_map[name] = logger
         # 输出到日志文件,按天分文件，保存30天
-        handler = logging.handlers.TimedRotatingFileHandler('logs/' + name + '.log', 'D', 1, 30)
+        handler = logging.handlers.TimedRotatingFileHandler('logs/' + name + '.' + str(os.getpid()) + '.log', 'D', 1, 30)
         handler.suffix = "%Y-%m-%d.log"
         handler.setLevel(logging.INFO)
         handler.setFormatter(logging.Formatter(LOG_FORMAT))
---
+++ test_case/test_new_logger.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('..')
+from libs import commons
+
+commons.create_logger('test')
